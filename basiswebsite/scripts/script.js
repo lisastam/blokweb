3fcfc8b56306d397ed534dc4c679737dde5a3005
lisastam <90210017+lisastam@users.noreply.github.com>
2021-09-21 12:45:28 +0200

    Add files via upload

--- basiswebsite/scripts/script.js
+++ basiswebsite/scripts/script.js
@@ -1,2 +1,10 @@
 // JavaScript Document
 var pElement = document.querySelector("p");
+
+var menu = document.querySelector("header ul li:last-child");
+var nav = document.querySelector("main nav ul");
+
+function navigatie() {
+    nav.classList.toggle("nav");
+}
+menu.addEventListener("click", navigatie);
