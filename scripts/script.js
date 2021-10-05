501e4ed20904c56b907b252d59a05339e18e2402
lisastam <90210017+lisastam@users.noreply.github.com>
2021-10-05 19:41:46 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,6 @@ var menu1 = document.querySelector("header ul li:nth-child(2)");
 var menu2 = document.querySelector("header ul li:nth-child(3)");
 var menu3 = document.querySelector("header ul li:last-child");
 
-var shirt = document.querySelector("main img:nth-child(3)")
 
 var nav = document.querySelector("main nav ul");
 
@@ -19,9 +18,3 @@ function navigatie() {
 menu1.addEventListener("click", navigatie);
 menu2.addEventListener("click", navigatie);
 menu3.addEventListener("click", navigatie);
-
-/*
-function animatie() {
-    shirt.classList.toggle("scrol")
-}
-shirt.addEventListener("scroll", animatie) */
